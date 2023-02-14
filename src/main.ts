5e4dd43e3b7dd5e3affaef43fda2effcc05e9db2
qiuhk <qiuhk@jiguang.cn>
2023-02-14 15:22:02 +0800

    chore: husky lint-staged commintlint

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,4 @@ import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 
-createApp(App).use(createPinia()).use(createPinia()).use(router)
-  .mount('#app');
+createApp(App).use(createPinia()).use(createPinia()).use(router).mount('#app');
